0d3525b14a6801eb1fce03e8d9599900d0cad8aa
Christian Schladetsch <christian.schladetsch@gmail.com>
2015-01-28 15:15:57 +1100

    Add base scripts and scene.

---
+++ Assets/Scripts/MarioCamera.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarioCamera : MonoBehaviour
+{
+	
+	void Awake()
+	{
+	}
+
+	void Start()
+	{
+	}
+
+	void Update()
+	{
+	}
+}
+
+

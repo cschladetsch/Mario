225225e4880ec9566a3a4da88791849a113166a0
Christian Schladetsch <christian.schladetsch@gmail.com>
2015-02-04 21:16:26 +1100

    Added idea of generic pickups.

---
+++ Assets/Scripts/PickupSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupSpawner : MonoBehaviour
+{
+	public float MinSpawnTime = 3;
+	public float MaxSpawnTime = 6;
+	public float Priority = 1;
+	public GameObject Prefab;
+	
+	void Awake()
+	{
+	}
+
+	void Start()
+	{
+	}
+
+	void Update()
+	{
+	}
+}
+
+

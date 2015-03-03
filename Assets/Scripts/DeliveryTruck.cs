f8959c1374883e9c4ab467826d0102a3d6373e3c
Christian Schladetsch <christian.schladetsch@gmail.com>
2015-03-03 14:14:59 +1100

    .

--- Assets/Scripts/DeliveryTruck.cs
+++ Assets/Scripts/DeliveryTruck.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class DeliveryTruck : MarioObject
 {
+	public bool Ready;
+
 	protected override void Construct()
 	{
 		base.Construct();
@@ -19,15 +19,47 @@ public class DeliveryTruck : MarioObject
 		base.BeforeFirstUpdate();
 	}
 
+	private bool _delivering;
+
+	private float _endX, _speed;
+
+	public void Deliver(float startX, float endX, float time, float height, float depth)
+	{
+		_delivering = true;
+		transform.position = new Vector3(startX, height, depth);
+		_endX = endX;
+		_speed = (_endX - startX)/time;
+
+		Debug.Log("Delivering truck");
+	}
+
 	protected override void Tick()
 	{
 		base.Tick();
 
-		RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+		if (_delivering)
+		{
+			var delta = DeltaTime*_speed;
+			transform.SetX(transform.position.x + delta);
 
+			Debug.Log(transform.position.x);
+
+			Ready = transform.position.x >= _endX;
+			if (Ready)
+				_delivering = false;
+		}
+
+		if (!Ready)
+			return;
+
+		if (!Input.GetMouseButtonDown(0)) 
+			return;
+
+		var hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
 		if (hit.collider != null)
 		{
 			Debug.Log("Target Position: " + hit.collider.gameObject.transform.position);
+			World.BeginArea(2);
 		}
 	}
 }

a5ac36076aae8cd44368981620f83a4fce7577c8
Christian Schladetsch <christian.schladetsch@gmail.com>
2015-03-13 23:01:31 +1100

    Adding generalised cooking area with scrollable, automatically generated Recipes.

---
+++ Assets/Scripts/TestCooker.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class TestCooker : MarioObject
+{
+	protected override void Construct()
+	{
+	}
+
+	protected override void Begin()
+	{
+	}
+
+	protected override void Tick()
+	{
+	}
+
+	public void Pressed(GameObject go)
+	{
+		Debug.Log("PRessed on " + go.name);
+	}
+}

f5e02df4e983d33af852faf3c9dd280c5f14c504
Christian Schladetsch <christian.schladetsch@gmail.com>
2015-03-25 21:46:37 +1100

    Revert "."
    This reverts commit 62486225056a31ecd54f79058dbb159735a4c9ce.

--- Assets/Scripts/MakeGlow.cs
+++ Assets/Scripts/MakeGlow.cs
@@ -1,62 +1,23 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
-/// <summary>
-/// Make something "glow" by ping-pong interpolating between two colors.
-/// 
-/// First, it looks for a button and if it finds one, changes the "normal" color
-/// of the button.
-/// 
-/// Next, it looks for an Image.
-/// 
-/// The colors change using a sinusoidal function to make it smooth.
-/// </summary>
 public class MakeGlow : MarioObject
 {
-	/// <summary>
-	/// The first color
-	/// </summary>
 	public Color From = Color.yellow;
-
-	/// <summary>
-	/// The color to change to
-	/// </summary>
 	public Color To = Color.white;
-
-	/// <summary>
-	/// How long to change from one color to the other
-	/// </summary>
 	public float Interval = 1;
 
-	/// <summary>
-	/// How long in current transition
-	/// </summary>
 	private float _timer;
-
-	/// <summary>
-	/// If true, we're going From color To color
-	/// </summary>
 	private bool _forward;
-
-	/// <summary>
-	/// The image color we're changing
-	/// </summary>
 	private Image _image;
-
-	/// <summary>
-	/// The button color we're changing. If there is no Image or no Button found, the
-	/// script self-destructs.
-	/// </summary>
 	private Button _button;
 
 	protected override void Construct()
 	{
 		_button = GetComponent<Button>();
 		if (_button == null)
-		{
 			_image = GetComponent<Image>();
-			return;
-		}
 
 		if (!_button && !_image)
 			Destroy(this);
@@ -77,9 +38,7 @@ public class MakeGlow : MarioObject
 			_timer = 0;
 		}
 
-		var t = Mathf.Sin(_timer/Interval);
-
-		var color = _forward ? Color.Lerp(From, To, t) : Color.Lerp(To, From, t);
+		var color = _forward ? Color.Lerp(From, To, _timer) : Color.Lerp(To, From, _timer);
 		if (_button)
 		{
 			var cb = _button.colors;
